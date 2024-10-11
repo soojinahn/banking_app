99bb2fe05916b32f0004d939bae9be5852f476d0
Soojin Ahn <sa2326@njit.edu>
2024-10-11 15:54:56 +0900

    Merging unnecessary branches

--- app/main.py
+++ app/main.py
@@ -24,12 +24,10 @@ def post_user(user:schemas.UserCreate, db:Session=Depends(get_db)):
         raise HTTPException(status_code=400, detail="Email already registered")
     return crud.create_user(db=db,user=user)
 
-
 @app.get("/users/", response_model=list[schemas.User])
 def get_users(skip:int=0, limit:int=0, db:Session=Depends(get_db)):
     users = crud.get_users(db,skip=skip,limit=limit)
     return users
-
 
 @app.get("/users/{user_id}/",response_model=schemas.User)
 def get_user(user_id:int, db:Session=Depends(get_db)):
