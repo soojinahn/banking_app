0d6f8e3089a0b1a3a3dfa40b3d9e8916a46f7fe3
Soojin Ahn <sa2326@njit.edu>
2024-10-12 16:53:05 +0900

    Updated all variables from User to Customer

--- app/crud.py
+++ app/crud.py
@@ -2,37 +2,38 @@ from sqlalchemy.orm import Session
 
 from . import models,schemas
 
-
-def get_user(db: Session, user_id: int):
-    return db.query(models.Customer).filter(models.Customer.id == user_id).first()
-
-
-def get_user_by_email(db: Session, email: str):
-    return db.query(models.Customer).filter(models.Customer.email == email).first()
-
-
-def get_users(db: Session, skip:int=0, limit:int=100):
-    # return db.query(models.User).offset(skip).limit(limit).all()
-    return db.query(models.Customer).offset(skip).limit(limit).all()
-
-
-def create_user(db: Session, user:schemas.CustomerCreate):
-    db_user = models.Customer(email=user.email,
-                          name=user.name,
-                          pin=user.pin)
+def create_customer(db: Session, customer:schemas.CustomerCreate):
+    db_user = models.Customer(email=customer.email,
+                          name=customer.name,
+                          pin=customer.pin)
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
     return db_user
 
 
-def get_accounts(db: Session, skip:int=0, limit: int=100):
-    return db.query(models.Account).offset(skip).limit(limit).all()
+def get_customer(db: Session, customer_id: int):
+    return db.query(models.Customer).filter(models.Customer.id == customer_id).first()
 
 
-def create_user_account(db:Session, account:schemas.AccountCreate, user_id : int):
-    db_account = models.Account(**account.model_dump(),owner_id=user_id )
+def get_customer_by_email(db: Session, email: str):
+    return db.query(models.Customer).filter(models.Customer.email == email).first()
+
+
+def get_customers(db: Session, skip:int=0, limit:int=100):
+    # return db.query(models.User).offset(skip).limit(limit).all()
+    return db.query(models.Customer).offset(skip).limit(limit).all()
+
+
+def create_customer_account(db:Session, account:schemas.AccountCreate, customer_id : int):
+    db_account = models.Account(**account.model_dump(),owner_id=customer_id )
     db.add(db_account)
     db.commit()
     db.refresh(db_account)
     return db_account
+
+
+def get_accounts(db: Session, skip:int=0, limit: int=100):
+    return db.query(models.Account).offset(skip).limit(limit).all()
+
+
--- app/main.py
+++ app/main.py
@@ -16,41 +16,41 @@ def get_db():
     finally:
         db.close()
 
-@app.post("/users/",response_model=schemas.Customer)
-def post_user(user:schemas.CustomerCreate, db:Session=Depends(get_db)):
-    db_user = crud.get_user_by_email(db, email=user.email)
+@app.post("/customers/",response_model=schemas.Customer)
+def post_customer(customer:schemas.CustomerCreate, db:Session=Depends(get_db)):
+    db_user = crud.get_customer_by_email(db, email=customer.email)
     if db_user:
         raise HTTPException(status_code=400, detail="Email already registered")
-    return crud.create_user(db=db,user=user)
+    return crud.create_customer(db=db,customer=customer)
 
-@app.get("/users/", response_model=list[schemas.Customer])
-def get_users(skip:int=0, limit:int=5, db:Session=Depends(get_db)):
-    users = crud.get_users(db,skip=skip,limit=limit)
-    return users
+@app.get("/customers/", response_model=list[schemas.Customer])
+def get_customers(skip:int=0, limit:int=5, db:Session=Depends(get_db)):
+    customers = crud.get_customers(db,skip=skip,limit=limit)
+    return customers
 
-@app.get("/users/{user_id}/",response_model=schemas.Customer)
-def get_user(user_id:int, db:Session=Depends(get_db)):
-    db_user = crud.get_user(db,user_id =user_id )
+@app.get("/customers/{customer_id}/",response_model=schemas.Customer)
+def get_customer(customer_id:int, db:Session=Depends(get_db)):
+    db_user = crud.get_customer(db,customer_id =customer_id )
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
     return db_user
 
 
-@app.post("/users/{user_id}/accounts/",response_model=schemas.Account)
-def post_account_for_user(user_id:int, account:schemas.AccountCreate, db:Session=Depends(get_db)):
-    return crud.create_user_account(db=db,user_id=user_id, account=account)
+@app.post("/customers/{customer_id}/accounts/",response_model=schemas.Account)
+def post_account_for_customer(customer_id:int, account:schemas.AccountCreate, db:Session=Depends(get_db)):
+    return crud.create_customer_account(db=db,customer_id=customer_id, account=account)
 
 
-@app.get("/users/{user_id}/accounts/", response_model=list[schemas.Account])
-def get_user_accounts(user_id:int,db:Session=Depends(get_db)):
-    customer = crud.get_user(db,user_id=user_id)
+@app.get("/customers/{customer_id}/accounts/", response_model=list[schemas.Account])
+def get_customer_accounts(customer_id:int,db:Session=Depends(get_db)):
+    customer = crud.get_customer(db,customer_id=customer_id)
     if customer is None:
         raise HTTPException(status_code=404, detail="User not found")
     return customer.accounts
 
 @app.get("/accounts/", response_model=list[schemas.Account])
 def get_accounts(skip:int=0,limit:int=100,db:Session=Depends(get_db)):
-    todos = crud.get_accounts(db,skip=skip,limit=limit)
-    return todos
+    accounts = crud.get_accounts(db,skip=skip,limit=limit)
+    return accounts
 
 
