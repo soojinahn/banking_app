dd6e5bbeba3b1a35323ed7eb80b0e0955ba35686
Soojin Ahn <sa2326@njit.edu>
2024-10-14 18:10:58 +0900

    Added React starter code

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          This is my react starter
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
